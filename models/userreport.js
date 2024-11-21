86615ebf801d4d22ffd68f4febaa2ec04376ffa2
Justice Liassidzi <juzkidofficial009@gmail.com>
2024-11-21 04:16:31 +0000

    models making changes to userreport on maps

--- models/userreport.js
+++ models/userreport.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 
 const userReportSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    dateReported: { type: String, required: true },
+    dateReported: { type: String },
     description: { type: String, required: true },
     email: { type: String, required: false},
     image: { type: String, required: true },
     location: { type: String, required: true },
-    map: { type: String, required: true }
+    map: { type: String }
 },
 { timestamps: true }
 );
